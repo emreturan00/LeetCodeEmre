66379858b7ffd7d42c292f4a6c387f0410382d3a
Emre Turan <93795815+emreturan00@users.noreply.github.com>
2023-03-22 12:47:16 +0300

    recursion

--- pascal_recursion.cpp
+++ pascal_recursion.cpp
@@ -2,11 +2,9 @@
 EMRE TURAN
 ID: 20191709010
 
-Time Complexity: O(n*logn)          one while loop which is connected to the input, however the relationship is less than linear
-                                    since it increments less than the input "num"
-                                    so, O(n*logn)
+Time Complexity: O(n!)          
 
-Space Complexity: O(1)          no additional space consuming during runtime since the  ints were already set.
+Space Complexity: O(n)          stack frames are allocated every time function recursed so n recursions = n stack frames
 
 */
 
@@ -16,35 +14,46 @@ Space Complexity: O(1)          no additional space consuming during runtime sin
 using namespace std;
 class Solution {
 public:
-    vector<int> getRow(int rowIndex,vector<int> prev) {
+    vector<int> getRow(int rowIndex, int counter, vector<int> prev) {
         vector<int> row;
-        
-        int sum = 0;
-        int counter =0;
 
-        if(counter == rowIndex)
+        int sum = 0;
+
+        if (counter == rowIndex)
         {
-            return row;
+            return prev;
         }
-        else{
-            prev_row() 
+        else
+        {
             row.push_back(1);       //left edge 1 before the middle numbers
             if (counter > 0)
             {
                 for (int j = 0; j < counter - 1; j++)
                 {
-                        //finding the sum which are current and previous indexes of previous row
-                    sum = prev_row[j] + prev_row[j+1];    
-                                                //push the sum to the triangle
+                    //finding the sum which are current and previous indexes of previous row
+                    sum = prev[j] + prev[j + 1];
+                    //push the sum to the triangle
                     row.push_back(sum);
                 }
                 row.push_back(1);   //right edge 1 after the middle numbers
             }
             counter++;
-            return getRow(counter);
-            }
-    }   
+            return getRow(rowIndex, counter, row);
         }
-           
-    
+    }
 };
+
+
+int main()
+{
+    Solution emre;
+
+    vector<int>arr;
+
+    vector <int> res = emre.getRow(9, 0, arr);
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+}
